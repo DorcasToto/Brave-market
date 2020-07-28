2f7d35378e1d67250b59557adc7324caaafdf746
Moringa School <dorcasmosonik@gmail.com>
2020-07-28 21:37:30 +0300

    added navigational bar

---
+++ modules/js/home.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myNavBar");
+    if (x.className === "navBar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navBar";
+    }
+  }
+  
